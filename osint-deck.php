e81a232f25d821498649fbf680449d454747a8f5
Quintana <guillermo.quintana@cencosud.com.ar>
2026-04-06 23:53:28 -0300

    chore(release): update  //silence is gold

--- osint-deck.php
+++ osint-deck.php
@@ -3,7 +3,7 @@
  * Plugin Name: OSINT Deck
  * Plugin URI: https://osintdeck.github.io
  * Description: Mazo OSINT para WordPress: catálogo de herramientas, motor de decisión por indicadores (IoC), shortcodes, métricas, integraciones (Google, Turnstile) y panel de administración. Incluye logs opcionales para diagnóstico.
- * Version: 1.0.2
+ * Version: 1.0.21
  * Author: Equipo OSINT Deck
  * Author URI: https://github.com/OsintDeck
  * License: GPL2
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'OSINT_DECK_VERSION', '1.0.2' );
+define( 'OSINT_DECK_VERSION', '1.0.21' );
 define( 'OSINT_DECK_PLUGIN_FILE', __FILE__ );
 define( 'OSINT_DECK_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OSINT_DECK_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
